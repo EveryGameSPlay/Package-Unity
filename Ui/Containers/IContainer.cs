65f32e1c5bd193ac10d6db82dd7ada677d248058
EveryGameSPlay <repsbro@gmail.com>
2021-01-27 20:15:35 +0300

    Added Containers.

---
+++ Ui/Containers/IContainer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Egsp.Core
+{
+    
+    public interface IContainer
+    {
+        /// <summary>
+        /// Создает экземпляр объекта, помещает его в контейнер и возвращает на него ссылку.
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        TObject PutPrefab<TObject>(TObject prefab) where TObject : MonoBehaviour;
+
+        /// <summary>
+        /// Помещает уже созданный экземпляр в контейнер и возвращает на него ссылку.
+        /// </summary>
+        TObject Put<TObject>(TObject instance) where TObject : MonoBehaviour;
+    }
+}
---
+++ Ui/Containers/TransformContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Egsp.Utils.GameObjectUtilities;
+using UnityEngine;
+
+namespace Egsp.Core
+{
+    public class TransformContainer : MonoBehaviour, IContainer
+    {
+        public bool worldPositionStays;
+
+        private List<object> _container = new List<object>();
+
+        public TObject PutPrefab<TObject>(TObject prefab) where TObject : MonoBehaviour
+        {
+            var inst = Instantiate(prefab);
+            inst.transform.SetParent(transform,worldPositionStays);
+            
+            _container.Add(inst);
+            return inst;
+        }
+
+        public TObject Put<TObject>(TObject instance) where TObject : MonoBehaviour
+        {
+            instance.transform.SetParent(transform, worldPositionStays);
+            
+            _container.Add(instance);
+            return instance;
+        }
+
+
+        public void Clear()
+        {
+            _container.Clear();
+            transform.DestroyAllChildrens();
+        }
+
+        public IEnumerable<TObject> GetEnumerable<TObject>()
+        {
+            return _container.Cast<TObject>();
+        }
+    }
+}
