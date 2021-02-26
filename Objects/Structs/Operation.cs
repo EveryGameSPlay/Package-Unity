ae763caaa1abd07bf8090ccb5cb2685f5eb9a4da
EveryGameSPlay <repsbro@gmail.com>
2021-02-26 18:43:29 +0300

    Added Operation struct for exception cases handling.

---
+++ Objects/Structs/Operation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Egsp.Core
+{
+    public readonly struct Operation<TResult>
+    {
+        public readonly OperationResultType Type;
+
+        public readonly TResult Result;
+
+        public readonly string ErrorMessage;
+
+        public bool Ok => Type == OperationResultType.Correct;
+
+        public Operation(TResult result)
+        {
+            Type = OperationResultType.Correct;
+            Result = result;
+            
+            ErrorMessage = String.Empty;
+        }
+
+        public Operation(string errorMessage)
+        {
+            Type = OperationResultType.Error;
+            ErrorMessage = errorMessage;
+
+            Result = default(TResult);
+        }
+        
+        public static Operation<TResult> Correct(TResult result)
+        {
+            return new Operation<TResult>(result);
+        }
+
+        public static Operation<TResult> Error(string errorMessage)
+        {
+            return new Operation<TResult>(errorMessage);
+        }
+    }
+    
+    public enum OperationResultType
+    {
+        Correct,
+        Error
+    }
+}
