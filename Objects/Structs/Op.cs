7b5706ae17410b6a813cf624750bca1797b614dc
EveryGameSPlay <repsbro@gmail.com>
2021-02-27 01:38:51 +0300

    Renamed Operation to Op and added non-generic version of this type.

---
+++ Objects/Structs/Op.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Egsp.Core
+{
+    public readonly struct Op
+    {
+        public readonly OperationResultType Type;
+        public readonly string ErrorMessage;
+        
+        public bool Ok => Type == OperationResultType.Correct;
+        public bool NotOk => Type != OperationResultType.Correct;
+
+        public Op(OperationResultType type,string errorMessage = null)
+        {
+            Type = type;
+            
+            if(errorMessage == null || type == OperationResultType.Correct)
+                ErrorMessage = String.Empty;
+            else
+                ErrorMessage = errorMessage;
+        }
+        
+        public Op(string errorMessage)
+        {
+            Type = OperationResultType.Error;
+            ErrorMessage = errorMessage;
+        }
+    }
+    
+    public readonly struct Op<TResult>
+    {
+        public readonly OperationResultType Type;
+
+        public readonly TResult Result;
+
+        public readonly string ErrorMessage;
+
+        public bool Ok => Type == OperationResultType.Correct;
+        public bool NotOk => Type != OperationResultType.Correct;
+
+        public Op(TResult result)
+        {
+            Type = OperationResultType.Correct;
+            Result = result;
+            
+            ErrorMessage = String.Empty;
+        }
+
+        public Op(string errorMessage)
+        {
+            Type = OperationResultType.Error;
+            ErrorMessage = errorMessage;
+
+            Result = default(TResult);
+        }
+        
+        public static Op<TResult> Correct(TResult result)
+        {
+            return new Op<TResult>(result);
+        }
+
+        public static Op<TResult> Error(string errorMessage)
+        {
+            return new Op<TResult>(errorMessage);
+        }
+    }
+    
+    public enum OperationResultType
+    {
+        Correct,
+        Error
+    }
+}
