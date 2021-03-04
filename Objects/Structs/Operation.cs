101e7af8290655bfa218abf8452eaa9fa3aae0f1
EveryGameSPlay <repsbro@gmail.com>
2021-03-04 19:35:02 +0300

    Improved operation type and added option type (like option in F#)

---
+++ Objects/Structs/Operation.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+namespace Egsp.Core
+{
+    /// <summary>
+    /// Объект-результат выполнения операции.
+    /// Нужен для более мягкой обработки исключений.
+    /// Может быть в двух состояниях: корректном и с ошибкой.
+    /// Применение: получение состояния выполнения процедуры.
+    /// </summary>
+    public readonly struct Operation
+    {
+        public readonly OperationResultType Type;
+        public readonly string ErrorMessage;
+        
+        public bool Ok => Type == OperationResultType.Correct;
+        public bool NotOk => Type != OperationResultType.Correct;
+
+        public Operation(OperationResultType type,string errorMessage = null)
+        {
+            Type = type;
+            
+            if(errorMessage == null || type == OperationResultType.Correct)
+                ErrorMessage = String.Empty;
+            else
+                ErrorMessage = errorMessage;
+        }
+        
+        public Operation(string errorMessage)
+        {
+            Type = OperationResultType.Error;
+            ErrorMessage = errorMessage;
+        }
+    }
+    
+    /// <summary>
+    /// Объект-результат выполнения операции.
+    /// Нужен для более мягкой обработки исключений.
+    /// Может быть в двух состояниях: корректном и с ошибкой.
+    /// Применение: получение состояния выполнения процедуры.
+    /// </summary>
+    public readonly struct Operation<TResult>
+    {
+        public readonly OperationResultType Type;
+
+        public readonly TResult Result;
+
+        public readonly string ErrorMessage;
+
+        public bool Ok => Type == OperationResultType.Correct;
+        public bool NotOk => Type != OperationResultType.Correct;
+
+        public Operation(TResult result)
+        {
+            Type = OperationResultType.Correct;
+            Result = result;
+            
+            ErrorMessage = String.Empty;
+        }
+
+        public Operation(string errorMessage)
+        {
+            Type = OperationResultType.Error;
+            ErrorMessage = errorMessage;
+
+            Result = default(TResult);
+        }
+        
+        public static Operation<TResult> Correct(TResult result)
+        {
+            return new Operation<TResult>(result);
+        }
+
+        public static Operation<TResult> Error(string errorMessage)
+        {
+            return new Operation<TResult>(errorMessage);
+        }
+    }
+    
+    /// <summary>
+    /// Объект-результат выполнения операции.
+    /// Нужен для более мягкой обработки исключений.
+    /// Может быть в двух состояниях: корректном и с ошибкой.
+    /// Применение: получение состояния выполнения процедуры.
+    /// </summary>
+    public readonly struct Operation<TResult, TError> 
+    {
+        public readonly OperationResultType Type;
+
+        public readonly TResult Result;
+
+        public readonly TError ErrorValue;
+
+        public bool Ok => Type == OperationResultType.Correct;
+        public bool NotOk => Type != OperationResultType.Correct;
+
+        public Operation(TResult result)
+        {
+            Type = OperationResultType.Correct;
+            Result = result;
+
+            ErrorValue = default(TError);
+        }
+
+        public Operation(TError errorValue)
+        {
+            Type = OperationResultType.Error;
+            ErrorValue = errorValue;
+
+            Result = default(TResult);
+        }
+        
+        public static Operation<TResult, TError> Correct(TResult result)
+        {
+            return new Operation<TResult, TError>(result);
+        }
+
+        public static Operation<TResult, TError> Error(TError errorValue)
+        {
+            return new Operation<TResult, TError>(errorValue);
+        }
+
+        public static explicit operator Operation(Operation<TResult, TError> operation)
+        {
+            if (operation.Ok)
+                return new Operation(OperationResultType.Correct);
+            else
+                return new Operation(operation.ErrorValue.ToString());
+        }
+
+        public static explicit operator Operation<TResult>(Operation<TResult, TError> operation)
+        {
+            if (operation.Ok)
+                return new Operation<TResult>(operation.Result);
+            else
+                return Operation<TResult>.Error(operation.ErrorValue.ToString());
+        }
+    }
+    
+    public enum OperationResultType
+    {
+        Correct,
+        Error
+    }
+
+    public abstract class OperationError{ }
+
+    public sealed class MessageError : OperationError
+    {
+        private readonly string _message;
+
+        public MessageError(string message)
+        {
+            _message = message;
+        }
+
+        public override string ToString()
+        {
+            return _message;
+        }
+    }
+    
+}
---
+++ Objects/Structs/Option.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Egsp.Core
+{
+    public readonly struct Option<TValue>
+    {
+        private static Option<TValue> _none = new Option<TValue>();
+        public static ref readonly Option<TValue> None => ref _none;
+
+        /// <summary>
+        /// Имеется ли значение.
+        /// </summary>
+        public readonly bool IsSome;
+        
+        private readonly TValue _value;
+        
+        public TValue Value
+        {
+            get
+            {
+                if (IsSome)
+                    return _value;
+                else
+                    throw new OptionNoneValueAccessException();
+            }
+        }
+
+        public Option(TValue value)
+        {
+            _value = value;
+            IsSome = true;
+        }
+        
+        /// <summary>
+        /// Создает новый экземпляр со значением. Альтернатива new OptionT(value).
+        /// </summary>
+        public static Option<TValue> Some(TValue value) => new Option<TValue>(value);
+
+        public static explicit operator TValue(Option<TValue> optional)
+        {
+            return optional.Value;
+        }
+        
+        public static implicit operator Option<TValue>(TValue value)
+        {
+            return new Option<TValue>(value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Option<TValue>)
+                return this.Equals((Option<TValue>)obj);
+            else
+                return false;
+        }
+        public bool Equals(Option<TValue> other)
+        {
+            if (IsSome && other.IsSome)
+                return object.Equals(_value, other._value);
+            else
+                return IsSome == other.IsSome;
+        }
+        
+        public class OptionNoneValueAccessException : Exception
+        {
+        }
+    }
+}
