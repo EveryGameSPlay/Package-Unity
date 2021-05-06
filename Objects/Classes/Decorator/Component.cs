9b0fc7aaafb79bc8006474a1ea9abf827110307d
EveryGameSPlay <repsbro@gmail.com>
2021-05-06 17:58:33 +0300

    Added Decorator and Component classes for complex objcets.

---
+++ Objects/Classes/Decorator/Component.cs
@@ -0,0 +1,37 @@
+﻿namespace Egsp.CSharp
+{
+    public abstract class Component
+    {
+        public DecoratorBase Parent { get; private set; }
+
+        public static TComponent Create<TComponent>(DecoratorBase parent)
+            where TComponent : Component, new()
+        {
+            var component = new TComponent();
+
+            component.Parent = parent;
+            return component;
+        }
+
+        public static void Attach(Component component, DecoratorBase parent)
+        {
+            if (component.Parent == null)
+            {
+                component.Parent = parent;
+            }
+        }
+
+        public static void Detach(Component component, DecoratorBase parent)
+        {
+            if (parent == component.Parent)
+            {
+                component.Parent = null;
+            }
+        }
+    }
+
+    public interface IInvokableComponent
+    {
+        void Invoke();
+    }
+}
---
+++ Objects/Classes/Decorator/DecoratorBase.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Egsp.Core;
+
+namespace Egsp.CSharp
+{
+    public abstract class DecoratorBase : IEnumerable<Component>
+    {
+        private Dictionary<Type, Component> Components { get; set; } = new Dictionary<Type, Component>();
+        
+        public AddComponentResult AddComponent<TComponent>(TComponent component)
+            where TComponent : Component
+        {
+            var type = typeof(TComponent);
+            if (Components.ContainsKey(type))
+                return AddComponentResult.NotAdded;
+            
+            AddComponentInternal(type,component);
+            return AddComponentResult.Added;
+        }
+
+        private void AddComponentInternal<TComponent>(Type type,TComponent component)
+            where TComponent : Component
+        {
+            Component.Attach(component, this);
+            Components.Add(type, component);
+        }
+
+        public RemoveComponentResult Remove<TComponent>()
+            where TComponent : Component, new()
+        {
+            var type = typeof(TComponent);
+            if (Components.TryGetValue(type, out var component))
+            {
+                RemoveComponentInternal(type, component);
+                return RemoveComponentResult.Removed;
+            }
+
+            return RemoveComponentResult.NotFound;
+        }
+
+        private void RemoveComponentInternal<TComponent>(Type type, TComponent component)
+            where TComponent : Component
+        {
+            Components.Remove(type);
+            Component.Detach(component, this);
+        }
+
+        public Option<TComponent> GetComponent<TComponent>()
+            where TComponent : Component, new()
+        {
+            if (Components.TryGetValue(typeof(TComponent), out var component))
+            {
+                return component as TComponent;
+            }
+
+            return Option<TComponent>.None;
+        }
+        
+        public IEnumerator<Component> GetEnumerator()
+        {
+            foreach (var keyValuePair in Components)
+            {
+                yield return keyValuePair.Value;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public enum AddComponentResult
+        {
+            Added,
+            NotAdded
+        }
+
+        public enum RemoveComponentResult
+        {
+            Removed,
+            NotFound
+        }
+    }
+}
