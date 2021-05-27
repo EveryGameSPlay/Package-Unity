4b3ea44e64f1ab75b8c4baa29a43870323c7fc9d
EveryGameSPlay <repsbro@gmail.com>
2021-05-27 18:23:01 +0300

    NotNull<TValue> type for not null parameters.

---
+++ Objects/Structs/NotNull.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Egsp.Core
+{
+    /// <summary>
+    /// <para>При отсутствующем значении вызывает исключение на этапе создания экземпляра.</para>
+    /// <para>Пустой конструктор нельзя использовать!</para>
+    /// </summary>
+    public struct NotNull<TValue> where TValue : class
+    {
+        /// <summary>
+        /// Если данное значение null, значит кто-то не использовал пустой конструктор.
+        /// </summary>
+        public readonly TValue Value;
+
+        public NotNull(TValue value)
+        {
+            Value = value ?? throw new NullReferenceException();
+        }
+
+        public static explicit operator TValue(NotNull<TValue> optional)
+        {
+            return optional.Value;
+        }
+        
+        public static implicit operator NotNull<TValue>(TValue value)
+        {
+            return new NotNull<TValue>(value);
+        }
+    }
+}
